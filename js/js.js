bd88cac46f2adb0101948ed9b1cb8bbe627c31d1
yun <gl75@naver.com>
2019-04-30 22:31:04 +0900

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,9 @@
 function encode(){
     var input = document.getElementById("original").value;
-    input = "hi";
-    document.getElementById("original").value = text;
+    var input_l = input.split('');
+    for(i=0; i<input_l.length; i++){
+        input_l[i] = input.charCodeAt(i);
+    }
+    output = input_l.join('');
+    document.getElementById("original").value = output;
 }

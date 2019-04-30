ee17bab98170d099ffee083f9e1be74b0984a717
yun <gl75@naver.com>
2019-05-01 08:59:11 +0900

    encode algo version up

--- js/js.js
+++ js/js.js
@@ -1,3 +1,12 @@
+var key1 = "리코가세상에서제일좋아"; key1_l = key1.split('');
+var key2 = "세상에서리코가제일이뻐"; key2_l = key2.split('');
+var key3 = "제일착한리코야사랑해요"; key3_l = key3.split('');
+for(i=0; i<11; i++){
+    key1_l[i] = key1.charCodeAt(i);
+    key2_l[i] = key2.charCodeAt(i);
+    key3_l[i] = key3.charCodeAt(i);
+}
+
 function encode(){
     var input = document.getElementById("original").value;
     var input_l = input.split('');
@@ -6,7 +15,13 @@ function encode(){
         input_l[i] = input.charCodeAt(i);
 
         if((input_l[i] >= 44032) && (input_l[i] <= 55203)){
-            input_l[i] += 10000;
+            if(input_l[i]%3 == 0){
+                input_l[i] += key1_l[i%11];
+            }else if(input_l[i]%3 == 1){
+                input_l[i] += key2_l[i%11];
+            }else{
+                input_l[i] += key3_l[i%11];
+            }
 
             if(input_l[i] > 55203){
                 input_l[i] -= 11171;
@@ -27,7 +42,13 @@ function decode(){
         input_l[i] = input.charCodeAt(i);
 
         if((input_l[i] >= 44032) && (input_l[i] <= 55203)){
-            input_l[i] -= 10000;
+            if(input_l[i]%3 == 0){
+                input_l[i] -= key1_l[i%11];
+            }else if(input_l[i]%3 == 1){
+                input_l[i] -= key2_l[i%11];
+            }else{
+                input_l[i] -= key3_l[i%11];
+            }
 
             if(input_l[i] < 44032){
                 input_l[i] += 11171;

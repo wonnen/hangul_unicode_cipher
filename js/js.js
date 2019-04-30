256c81d5544f5226966cdf352445599be74c805d
yun <gl75@naver.com>
2019-04-30 22:52:30 +0900

    codec update

--- js/js.js
+++ js/js.js
@@ -1,9 +1,41 @@
 function encode(){
     var input = document.getElementById("original").value;
     var input_l = input.split('');
+
     for(i=0; i<input_l.length; i++){
         input_l[i] = input.charCodeAt(i);
+
+        if((input_l[i] >= 44032) && (input_l[i] <= 55215)){
+            input_l[i] += 10000;
+
+            if(input_l[i] > 55215){
+                input_l[i] -= 11183;
+            }
+        }
+        input_l[i] = String.fromCharCode(input_l[i]);
     }
+
+    output = input_l.join('');
+    document.getElementById("encoded").value = output;
+}
+
+function decode(){
+    var input = document.getElementById("encoded").value;
+    var input_l = input.split('');
+
+    for(i=0; i<input_l.length; i++){
+        input_l[i] = input.charCodeAt(i);
+
+        if((input_l[i] >= 44032) && (input_l[i] <= 55215)){
+            input_l[i] -= 10000;
+
+            if(input_l[i] < 44032){
+                input_l[i] += 11183;
+            }
+        }
+        input_l[i] = String.fromCharCode(input_l[i]);
+    }
+
     output = input_l.join('');
     document.getElementById("original").value = output;
 }
